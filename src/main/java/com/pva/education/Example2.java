7dd1555b3d8bc6dfc8a303fd2c38cfa15a602d9e
gkontonis <luisxd88@gmail.com>
2020-03-09 14:01:38 +0100

    changed code to be self documention

--- src/main/java/com/pva/education/Example2.java
+++ src/main/java/com/pva/education/Example2.java
@@ -5,22 +5,23 @@ import java.util.Scanner;
 
 public class Example2 {
 
-    public static void main (String [] args){
-        Example2 a2 = new Example2();
+    public static void main(String[] args) {
+        Example2 calculator = new Example2();
 
-        System.out.println(a2.calcHypo());
+        double cathetus1 = calculator.askForDouble("Cathetus 1: ");
+        double cathetus2 = calculator.askForDouble("Cathetus 2: ");
+        System.out.println("Result: " + calculator.calcHypothenuse(cathetus1, cathetus2));
     }
 
-    public double calcHypo(){
-        double cathetus1, cathetus2;
+    public double calcHypothenuse(double cathetus1, double cathetus2) {
+        return (Math.sqrt(Math.pow(cathetus1, 2) + Math.pow(cathetus2, 2)));
+        //ohne math sqrt und pow in bsp Sqrt
+    }
+
+    private double askForDouble(String msg) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Kathete 1: ");
-        cathetus1 = Double.parseDouble(sc.nextLine());
-        System.out.print("Kathete 2: ");
-        cathetus2 = Double.parseDouble(sc.nextLine());
-
-        return (Math.sqrt(Math.pow(cathetus1, 2) + Math.pow(cathetus2,2)));
+        System.out.print(msg);
+        return Double.parseDouble(sc.nextLine());
     }
 
 }
